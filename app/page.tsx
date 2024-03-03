01bb7e0bf3254171039b27cb1aebcb950697da90
fal3n-4ngel <adiadithyakrishnan@gmail.com>
2024-03-03 23:49:29 +0530

    fix: responsive legacy yt section

--- app/page.tsx
+++ app/page.tsx
@@ -39,10 +39,29 @@ export default function Home() {
       </Marquee>
       <section className="relative mt-11 flex  flex-col items-center justify-between">
         <div className="flex h-full flex-1 flex-col items-center justify-end">
-          <h1 className="heading">OUR LEGACY</h1>
-          <h2 className="heading flex gap-1 text-3xl text-white">
-            2005 - <Image width={60} height={60} src="/infinity.svg" alt="infinity" />
-          </h2>
+          <div className="relative flex flex-col items-center justify-center md:m-5">
+            <Image
+              src="/star.png"
+              alt=""
+              width={1080}
+              height={1080}
+              className="absolute -left-[40px] -top-[30px] h-[30px] w-[30px] "
+              style={{ animationDuration: '4s' }}
+            />
+
+            <h1 className="heading">OUR LEGACY</h1>
+            <h2 className="heading flex gap-1 text-3xl text-white">
+              2005 - <Image width={60} height={60} src="/infinity.svg" alt="infinity" />
+            </h2>
+            <Image
+              src="/star.png"
+              alt=""
+              width={1080}
+              height={1080}
+              className="absolute -bottom-[10px] -right-[20px] h-[30px] w-[30px] "
+              style={{ animationDuration: '4s' }}
+            />
+          </div>
           <p className="prose mt-6 text-center text-white">
             The Techno-Cultural festival of MBCET aka, Crossroads has become an integral
             part of the cultural calendar of Trivandrum since its launch in 2005. From
@@ -62,19 +81,19 @@ export default function Home() {
         />
       </section>
 
-      <section className=" relative flex  min-h-screen items-center justify-center ">
-        <div className="relative flex h-[700px] w-[70%] items-center justify-center ">
+      <section className=" relative flex  items-center justify-center md:min-h-screen ">
+        <div className="relative mx-auto flex h-[300px] items-center  justify-center md:h-[700px] md:w-[60%] ">
           <Image
             src="/legacy.png"
             alt=""
             width={1080}
             height={1080}
-            className="duraction-[3000] absolute left-0 top-0 h-[300px] w-[300px] animate-spin"
+            className="absolute left-0 top-0 h-[100px] w-[100px]  animate-spin md:h-[300px] md:w-[300px]"
             style={{ animationDuration: '4s' }}
           />
           <div className="z-[1] h-[200px] w-[100vw] md:h-[400px] md:w-[50vw] ">
             <iframe
-              className="h-full w-full rounded-xl "
+              className="mx-auto h-full w-[80%] rounded-xl "
               src="https://www.youtube.com/embed/QaXcVAkamxc"
               title="CROSSROADS&#39;23 | After Movie | Mar Baselios College of Engineering and Technology"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
@@ -85,17 +104,10 @@ export default function Home() {
             alt=""
             width={1080}
             height={1080}
-            className="duraction-[3000] absolute bottom-0 right-0 h-[300px] w-[300px] animate-spin"
+            className="absolute bottom-0 right-0 h-[100px] w-[100px]  animate-spin md:h-[300px] md:w-[300px]"
             style={{ animationDuration: '4s' }}
           />
         </div>
-        <Image
-          src="/wave.png"
-          alt=""
-          width={1080}
-          height={1080}
-          className="absolute bottom-0 z-[1] w-full rotate-180"
-        />
       </section>
     </main>
   );
