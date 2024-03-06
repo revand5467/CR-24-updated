3c47b5550acf2984d32a8a9fc0c78913ad285058
revand5467 <revandsunil5467@gmail.com>
2024-03-06 16:38:29 +0530

    dept done

--- app/components/Events.tsx
+++ app/components/Events.tsx
@@ -1,7 +1,7 @@
 // Events.tsx
+
 import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
-import styles from './Events.module.css';
 
 const Events = () => {
     const [width1, setWidth] = useState(0);
@@ -10,33 +10,30 @@ const Events = () => {
     useEffect(() => {
         if (typeof window !== 'undefined') {
             setWidth(window.innerWidth);
+            console.log(width1);
             setHeight(window.innerHeight);
         }
     }, []);
 
     return (
-        <div className={styles.container}>
-            <div className={styles.rectangle}></div>
-            <div className={styles.imageWrapper}>
-                <div className={styles.imageContainer}>
+        <div className="relative w-full h-full flex flex-col items-center justify-center">
+            <div className="relative flex items-center justify-center">
+            <div className="relative flex items-center justify-center">
+                <div className="w-[80%] relative">
                     <Image
                         src="/Campus.jpg"
                         alt="Campus Ambassador Tiger"
                         layout="responsive"
                         width={50}
                         height={50}
+                        className='z-20'
                     />
+                    <div className={`absolute top-10 ml-[${width1/2}px] bg-yellow-200 w-full h-full -z-10`}></div>
                 </div>
-                <div className={styles.imageContainer} style={{ marginTop: '5%' }}>
-                    <Image
-                        src="/Events.jpg"
-                        alt="Events Tiger"
-                        layout="responsive"
-                        width={50} /* Adjust the width of the image */
-                        height={50} /* Adjust the height of the image */
-                    />
                 </div>
+
             </div>
+
         </div>
     );
 }
--- app/components/trial.tsx
+++ app/components/trial.tsx
@@ -1,98 +1,3 @@
-// 'use client'
-// import './parallelogram.css'
-// import React, { useState, useEffect } from 'react'
-// import Image from 'next/image';
-// const events = [
-//   {
-//     name: "CSE Dept. Tech Fest ",
-//     imageLink: "/hash.png"
-//   },
-//   {
-//     name: "EEE Dept. Tech Fest",
-//     imageLink: "/adharva.svg"
-//   },
-//   {
-//     name: "ECE Dept. Tech Fest",
-//     imageLink: "/trydan.png"
-//   },
-//   {
-//     name: "CE Dept. Tech Fest",
-//     imageLink: "/adharva.svg"
-//   },
-// ];
-
-
-// const Parallelogram = () => {
-//   const [width2, setWidth] = useState(0);
-//   const [height2, setHeight] = useState(0);
-
-//   useEffect(() => {
-//       if (typeof window !== 'undefined') {
-//           setWidth(window.innerWidth);
-//           setHeight(window.innerHeight);
-//       }
-//   },[] );
-//   return (
-
-//        <div  className="w-[calc(vw)] h-[calc(vw)+200px] flex items-center justify-center mb-20 mt-20  ">
-//       <div className="flex sm:flex-row mb-11 gap-20 flex-col ">
-//       {
-//           events.map((e,index) => {
-
-//             return (
-//               <div key={index} className="relative  flex-col  sm:pl-3 ">
-//                 <div className="absolute top-5 right-12  mr-4 ms:right-0 sm:w-[100x] md:h-[100px] w-[30px] h-[30px]  z-30">
-//                    <Image
-//               src="/arrow.png"
-//               alt=""
-//               width={70}
-//               height={50}
-//               className=" mb-20 ml-10  h-[100%] w-[100%] md:mb-22 md:ml-6   md:h-[50px] md:w-[60px] z-30 top-4 left-10"
-
-//             />
-//                 </div>
-//                   <div  className={`parallelogram w-[calc(100vw/1.8)] h-[calc(100vh/3)] sm:w-[calc(100vw/5.5)]   sm:h-[calc(100vh/2.4)]  transform -skew-x-1 -skew-y-6 flex items-center justify-center `}>
-//                <div key={index} className={`relative w-[calc(100vw/2)] h-[calc(100vh/3.4)] flex sm:w-[calc(100vw/6)] sm:border-10  sm:h-[calc(100vh/2.6)] border-[#061A19]  transform -skew-x-0 -skew-y-4  items-end justify-bottom  bg-[#061A19]`}>
-//               <div className=' flex items-center justify-center'>
-
-//               <Image
-//               src={e.imageLink  }
-//               alt=""
-//               width={108}
-//               height={208}
-//               className=" mb-20 ml-10  h-[100%] w-[100%] md:mb-22 md:ml-6   md:h-[200px] md:w-[200px]"
-
-//             />
-//               </div>
-
-//                </div>
-//                </div>
-//                  {/* <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 sm:w-[calc(100vw/6)] h-[20px] bg-orange border-white z-30"  >
-
-//                 </div> */}
-
-
-//                  <div className="absolute pb-1  sm:mt-6  top-3/4 left-1/2 transform  -translate-x-1/2 -translate-y-1/2 sm:w-[calc(100vw/6)] sm:h-[30px] h-[20px] sm:ml-1  bg-orange border-white z-30 w-[calc(100vw/1.988)]">
-//                   <h1 className="text-center text-sm text-stone-50 sm:text-lg font-medium font-['Poppins']  tracking-tighter ">{e.name}</h1>
-//                  </div>
-
-//               </div>
-
-//              // <div key={index} className=" sm:w-[calc(100vw/6)] border-[20px]  sm:h-[calc(100vh/2)]  transform skew-x-12 bg-white z-0"></div>
-//             )
-//           })
-//         }
-
-//       </div>
-
-//     </div>
-
-
-
-//   )
-// }
-
-//export default Parallelogram
 'use client';
 import './parallelogram.css';
 import React, { useState, useEffect } from 'react';
@@ -102,19 +7,23 @@ import { useInView } from "react-intersection-observer";
 const events = [
   {
     name: "CSE Dept. Tech Fest",
-    imageLink: "/hash.png"
+    imageLink: "/hash.png",
+    styles:'mb-20 ml-10 md:w-[220px] md:mb-[140px] md:ml-[70px]  md:h-[280px] w-[120px] h-[147px]  lg:w-[177px] lg:h-[218px] lg:mb-22 lg:ml-12 lg:mb-[100px]'
   },
   {
     name: "EEE Dept. Tech Fest",
+    styles:"mb-14 ml-0 w-[204px] h-[204px] md:w-[350px] md:h-[350px] md:mb-[80px] md:ml-[10px] lg:w-[296px] lg:mr-[50px] lg:h-[296px] lg:mb-22 lg:ml-12 lg:mb-[52px]",
     imageLink: "/adharva.svg"
   },
   {
     name: "ECE Dept. Tech Fest",
+    styles:"mb-10 ml-0 w-[213px] h-[200px] md:w-[300px] md:mb-[100px] md:ml-[30px] lg:w-[308px] md:h-[350px] lg:h-[296px] lg:mb-22 lg:ml-2 lg:mb-[40px]",
     imageLink: "/trydan.png"
   },
   {
     name: "CE Dept. Tech Fest",
-    imageLink: "/adharva.svg"
+    imageLink: "/adharva.svg",
+    styles:"mb-14 ml-0 w-[204px] h-[204px] md:w-[350px] md:h-[350px] md:mb-[80px] md:ml-[10px] lg:w-[296px] lg:mr-[50px] lg:h-[296px] lg:mb-22 lg:ml-12 lg:mb-[52px]",
   },
 ];
 
@@ -124,7 +33,6 @@ const cardVariants1: Variants = {
   },
   onscreen: {
     y: 50,
-    //rotate: -10,
     transition: {
       type: "spring",
       bounce: 0.4,
@@ -133,7 +41,6 @@ const cardVariants1: Variants = {
   }
 };
 const cardVariants = {
-
   hidden: {
     opacity: 0,
     scale: 0.5,
@@ -145,7 +52,6 @@ const cardVariants = {
       type: 'spring',
       damping: 10,
       stiffness: 100,
-
     },
   },
 };
@@ -161,8 +67,8 @@ const Parallelogram = () => {
     triggerOnce: false
   });
   const loopAnimation = {
-    scale: [1, 1.05, 1], // Sequence of scales
-    transition: { duration: 3, ease: "easeOut", repeat: Infinity, type: 'tween' }, // Infinite loop
+    scale: [1, 1.05, 1],
+    transition: { duration: 3, ease: "easeOut", repeat: Infinity, type: 'tween' },
   };
   const [width2, setWidth] = useState(0);
   const [height2, setHeight] = useState(0);
@@ -171,73 +77,53 @@ const Parallelogram = () => {
       controls.start("visible");
     }
   }, [controls, inView]);
-  // useEffect(() => {
-  //   if (typeof window !== 'undefined') {
-  //     setWidth(window.innerWidth);
-  //     setHeight(window.innerHeight);
-  //   }
-  // },[] );
 
   return (
-    <div className="w-[calc(vw)] h-[calc(vw)+200px] flex items-center justify-center mb-20 mt-20">
-      <div className="flex sm:flex-row mb-11 gap-20 flex-col">
+    <div className="w-[calc(vw)]  h-[calc(vw)+200px] flex items-center  justify-center mb-20 mt-20">
+      <div className="flex lg:flex-row md:w-[500px]  md:items-center  md:justify-center  md:flex-col mb-11 gap-20 flex-col">
         {events.map((e, index) => (
           <motion.div
             key={index}
-            //onViewportEnter={}
-            className="relative flex-col sm:pl-3"
+            className="relative flex-col lg:pl-3"
             variants={cardVariants1}
             initial="offscreen"
             whileInView="onscreen"
-            // animate={inView ? "visible" : "hidden"
-            // }
-            //whileInView={{ scale: 1.2 }}
             viewport={{ once: true, amount: 0.8 }}
-            //onScroll={controls.start}
-            //transition={{ duration: 4 , delay:2 }}
             whileHover={{ scale: 1.2 }}
             exit="hidden"
             transition={{ duration: 0.2, ease: "easeOut" }}
-            // className="box"
             ref={ref}
           >
             <motion.div
               key={index}
-              // className="relative flex-col sm:pl-3"
-              //variants={squareVariants}
-              //initial="hidden"
               animate={inView ? (loopAnimation) : "hidden"}
-              //transition={{ duration: 4 , delay:2 }}
               whileHover={{ scale: 1.2 }}
-              //exit="hidden"
-              //  transition={{ repeat: Infinity, duration: 1, ease: "easeInOut" }}
-              // className="box"
               ref={ref}
             >
-              <div className="absolute top-5 right-12  mr-4 ms:right-0 sm:w-[100x] md:h-[100px] w-[30px] h-[30px]  z-30">
+              <div className="absolute top-5 right-12   mr-4 ms:right-0 lg:w-[100x] lg:h-[150px] md:w-[50px] md:h-[50px] w-[30px] h-[30px]  z-30">
                 <Image
                   src="/arrow.png"
                   alt=""
                   width={70}
                   height={50}
-                  className=" mb-20 ml-10  h-[100%] w-[100%] md:mb-22 md:ml-6   md:h-[50px] md:w-[60px] z-30 top-4 left-10"
+                  className=" mb-20 ml-10  h-[100%] w-[100%] lg:mb-22 lg:ml-6   md:[50px] lg:h-[50px] lg:w-[60px] z-30 top-4 left-10"
                 />
               </div>
-              <div className={`parallelogram  w-[calc(100vw/1.8)] h-[calc(100vh/3)] sm:w-[calc(100vw/5.5)]   sm:h-[calc(100vh/2.4)]  transform -skew-x-1 -skew-y-6 flex items-center justify-center `}>
-                <div key={index} className={`relative w-[calc(100vw/2)] h-[calc(100vh/3.4)] flex sm:w-[calc(100vw/6)] sm:border-10  sm:h-[calc(100vh/2.6)] border-[#061A19]  transform -skew-x-0 -skew-y-4  items-end justify-bottom  bg-[#061A19]`}>
-                  <div className=' flex items-center justify-center'>
+              <div className={`parallelogram md:w-[400px] md:h-[500px] w-[calc(100vw/1.8)] h-[calc(100vh/3)] lg:w-[calc(100vw/5.5)]   lg:h-[calc(100vh/2.4)]  transform -skew-x-1 -skew-y-6 flex items-center justify-center `}>
+                <div key={index} className={`relative md:w-[370px] md:h-[470px] w-[calc(100vw/2)] h-[calc(100vh/3.3)] flex lg:w-[calc(100vw/6)] lg:border-10  lg:h-[calc(100vh/2.6)] border-[#061A19]  transform -skew-x-0 -skew-y-4  items-end justify-bottom  bg-[#061A19]`}>
+                  <div className={`flex items-center justify-center ` }>
                     <Image
                       src={e.imageLink}
                       alt=""
-                      width={108}
-                      height={208}
-                      className=" mb-20 ml-10  h-[100%] w-[100%] md:mb-22 md:ml-6   md:h-[200px] md:w-[200px]"
+                      width={100}
+                      height={100}
+                      className={`${e.styles} `}
                     />
                   </div>
                 </div>
               </div>
-              <div className="absolute pb-1  sm:mt-6  top-3/4 left-1/2 transform  -translate-x-1/2 -translate-y-1/2 sm:w-[calc(100vw/6)]  sm:h-[30px] h-[20px] sm:-ml-[2.1px]   bg-orange border-white z-30 w-[calc(100vw/1.988)]">
-                <h1 className="text-center text-sm text-stone-50 sm:text-lg font-medium font-['Poppins']  tracking-tighter ">{e.name}</h1>
+              <div className="absolute pb-1 md:-ml-[2px] md:w-[calc(100vw/2.21)] lg:mt-6  top-3/4 left-1/2 transform  -translate-x-1/2 -translate-y-1/2 lg:w-[calc(100vw/6)]  lg:h-[30px] h-[20px] lg:-ml-[2.1px]   bg-orange border-white z-30 w-[calc(100vw/1.988)]">
+                <h1 className="text-center text-sm md:text-[20px] text-stone-50 lg:text-lg font-medium font-['Poppins']  tracking-tighter ">{e.name}</h1>
               </div>
             </motion.div>
           </motion.div>
@@ -248,5 +134,3 @@ const Parallelogram = () => {
 };
 
 export default Parallelogram;
-
-
