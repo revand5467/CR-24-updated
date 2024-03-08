059f7dfdcd8561b5a21f9e65afeeebe2a47b9b76
Diya Sundeep <diyasundeep64@gmail.com>
2024-03-08 21:37:39 +0530

    delay and duration

--- app/components/Sponsors.tsx
+++ app/components/Sponsors.tsx
@@ -39,7 +39,7 @@ const Sponsors = () => {
         <motion.div
           initial={{ opacity: 0 }}
           animate={inView ? { opacity: 1 } : {}}
-          transition={{ delay: 2 }}
+          transition={{ delay: 1.4, duration: 3 }}
           className=" absolute inset-0 z-50 flex flex-col justify-center  "
         >
           <div className="flex flex-row flex-wrap justify-center space-x-4">
@@ -95,13 +95,13 @@ const Sponsors = () => {
           className="absolute inset-0 z-10 flex flex-col items-center justify-center text-center text-4xl font-bold text-amber-600"
         >
           <div className=" text-white">Our</div>
-          <div className="mb-20">Sponsorsss</div>
+          <div className="mb-20">Sponsors</div>
         </motion.div>
         <div className="  flex  w-1/2 justify-end   text-4xl">
           <motion.div
             initial={{ x: 0, opacity: 1 }}
-            animate={inView ? { x: "-100%", opacity: 1 } : {}}
-            transition={{ duration: 3, delay: 0.1 }}
+            animate={inView ? { x: "-120%", opacity: 1 } : {}}
+            transition={{ duration: 2, delay: 0.2 }}
             exit={{ opacity: 0, transition: { duration: 2 } }}
             className="mt-20 flex w-full items-center justify-end"
           >
@@ -116,8 +116,8 @@ const Sponsors = () => {
         <div className=" flex w-1/2 justify-start text-4xl">
           <motion.div
             initial={{ x: 0, opacity: 1 }}
-            animate={inView ? { x: "100%", opacity: 1 } : {}}
-            transition={{ duration: 3, delay: 0.1 }}
+            animate={inView ? { x: "120%", opacity: 1 } : {}}
+            transition={{ duration: 2, delay: 0.2 }}
             exit={{ opacity: 0, transition: { duration: 2 } }}
             className=" mt-20 flex w-full items-center"
           >
