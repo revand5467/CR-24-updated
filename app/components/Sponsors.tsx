0c3385c9d6cc109a57edda1dcda7fef3fd6d9f5f
Diya Sundeep <diyasundeep64@gmail.com>
2024-03-08 20:45:12 +0530

    logos

--- app/components/Sponsors.tsx
+++ app/components/Sponsors.tsx
@@ -5,8 +5,15 @@ import { useRef } from "react";
 import { useInView } from "react-intersection-observer";
 import heartr from "../../public/Heartr.png";
 import Image from "next/image";
-import Reg from "../../public/Reg.png";
-
+import reg from "../../public/reg.png";
+import penta_hut from "../../public/penta_hut.png";
+import kims from "../../public/kims.png";
+import bajaj from "../../public/bajaj.png";
+import hll from "../../public/hll.png";
+import south_park from "../../public/south_park.png";
+import mpire from "../../public/mpire.png";
+import mot from "../../public/mot.png";
+import realme from "../../public/realme.png";
 const Sponsors = () => {
   const [ref, inView] = useInView({
     triggerOnce: false
@@ -21,36 +28,36 @@ const Sponsors = () => {
           initial={{ opacity: 0 }}
           animate={inView ? { opacity: 1 } : {}}
           transition={{ delay: 2, duration: 2 }}
-          className=" absolute inset-0 z-50 grid grid-cols-4 justify-center  px-8 md:grid-cols-8 "
+          className=" absolute inset-0 z-50 grid  grid-cols-4 justify-center px-8 md:grid-cols-8"
         >
-          <Image alt="Reg logo" src={Reg} className="px-2 md:px-0" />
-          <Image alt="Reg logo" src={Reg} className="px-2 md:px-0" />
-          <Image alt="Reg logo" src={Reg} className="px-2 md:px-0" />
-          <Image alt="Reg logo" src={Reg} className="px-2 md:px-0" />
-          <Image alt="Reg logo" src={Reg} className="px-2 md:px-0" />
-          <Image alt="Reg logo" src={Reg} className="px-2 md:px-0" />
-          <Image alt="Reg logo" src={Reg} className="px-2 md:px-0" />
-          <Image alt="Reg logo" src={Reg} className="px-2 md:px-0" />
-          <Image alt="Reg logo" src={Reg} className="px-2 md:px-0" />
-          <Image alt="Reg logo" src={Reg} className="px-2 md:px-0" />
-          <Image alt="Reg logo" src={Reg} className="px-2 md:px-0" />
-          <Image alt="Reg logo" src={Reg} className="px-2 md:px-0" />
-          <Image alt="Reg logo" src={Reg} className="px-2 md:px-0" />
-          <Image alt="Reg logo" src={Reg} className="px-2 md:px-0" />
-          <Image alt="Reg logo" src={Reg} className="px-2 md:px-0" />
-          <Image alt="Reg logo" src={Reg} className="px-2 md:px-0" />
-          <Image alt="Reg logo" src={Reg} className="px-2 md:px-0" />
-          <Image alt="Reg logo" src={Reg} className="px-2 md:px-0" />
-          <Image alt="Reg logo" src={Reg} className="px-2 md:px-0" />
-          <Image alt="Reg logo" src={Reg} className="px-2 md:px-0" />
-          <Image alt="Reg logo" src={Reg} className="px-2 md:px-0" />
-          <Image alt="Reg logo" src={Reg} className="px-2 md:px-0" />
-          <Image alt="Reg logo" src={Reg} className="px-2 md:px-0" />
-          <Image alt="Reg logo" src={Reg} className="px-2 md:px-0" />
-          <Image alt="Reg logo" src={Reg} className="px-2 md:px-0" />
-          <Image alt="Reg logo" src={Reg} className="px-2 md:px-0" />
-          <Image alt="Reg logo" src={Reg} className="px-2 md:px-0" />
-          <Image alt="Reg logo" src={Reg} className="px-2 md:px-0" />
+          <Image alt="Reg logo" src={reg} className=" px-2 md:px-0" />
+          <Image alt="Reg logo" src={penta_hut} className="px-2 md:px-0" />
+          <Image alt="Reg logo" src={kims} className="px-2 md:px-0" />
+          <Image alt="Reg logo" src={bajaj} className="px-2 md:px-0" />
+          <Image alt="Reg logo" src={hll} className="px-2 md:px-0" />
+          <Image alt="Reg logo" src={south_park} className="px-2 md:px-0" />
+          <Image alt="Reg logo" src={mpire} className="px-2 md:px-0" />
+          <Image alt="Reg logo" src={mot} className="px-2 md:px-0" />
+          <Image alt="Reg logo" src={realme} className="px-2 md:px-0" />
+          <Image alt="Reg logo" src={reg} className="px-2 md:px-0" />
+          <Image alt="Reg logo" src={reg} className="px-2 md:px-0" />
+          <Image alt="Reg logo" src={reg} className="px-2 md:px-0" />
+          <Image alt="Reg logo" src={reg} className="px-2 md:px-0" />
+          <Image alt="Reg logo" src={reg} className="px-2 md:px-0" />
+          <Image alt="Reg logo" src={reg} className="px-2 md:px-0" />
+          <Image alt="Reg logo" src={reg} className="px-2 md:px-0" />
+          <Image alt="Reg logo" src={reg} className="px-2 md:px-0" />
+          <Image alt="Reg logo" src={reg} className="px-2 md:px-0" />
+          <Image alt="Reg logo" src={reg} className="px-2 md:px-0" />
+          <Image alt="Reg logo" src={reg} className="px-2 md:px-0" />
+          <Image alt="Reg logo" src={reg} className="px-2 md:px-0" />
+          <Image alt="Reg logo" src={reg} className="px-2 md:px-0" />
+          <Image alt="Reg logo" src={reg} className="px-2 md:px-0" />
+          <Image alt="Reg logo" src={reg} className="px-2 md:px-0" />
+          <Image alt="Reg logo" src={reg} className="px-2 md:px-0" />
+          <Image alt="Reg logo" src={reg} className="px-2 md:px-0" />
+          <Image alt="Reg logo" src={reg} className="px-2 md:px-0" />
+          <Image alt="Reg logo" src={reg} className="px-2 md:px-0" />
         </motion.div>
 
         <motion.div
